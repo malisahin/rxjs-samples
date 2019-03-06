4cfa0c2d9c7ec334e17cbf5a31113744d4600016
ali.sahinogullari <ali.sahinogullari@etiya.com>
2019-03-06 16:27:30 +0300

    Merge Map

--- src/app/operators/combination/merge-map.service.ts
+++ src/app/operators/combination/merge-map.service.ts
@@ -1,23 +1,42 @@
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
-import { mergeMap } from 'rxjs/operators';
+import { from, of } from 'rxjs';
+import { delay, map, mergeAll, mergeMap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class MergeMapService {
-  constructor() {}
+  constructor() {
+  }
 
   execute() {
+    // this.sample1();
+    this.sample2();
+  }
+
+  sample1() {
     const source = of(['1', '2', '3', '4']);
 
     const example = source.pipe(
       mergeMap(value => {
         console.log(`${value}`);
         return of(`Order:  ${value}`);
-      }),
+      })
     );
 
     const subscribe = example.subscribe(val => console.log(val));
+  }
+
+  sample2() {
+
+    const getData = (param) => {
+      return of(`retrieved new data with param ${param}`)
+        .pipe(delay(1000));
+    };
+
+    from([1, 2, 3, 4]).pipe(
+      map(param => getData(param))
+    ).subscribe(value => value.subscribe(data => console.log(data)));
+
   }
 }
